e55386e9e30cc95a1087f1c486b68ff45f0b54dc
jior2008 <jior2008@163.com>
2019-10-04 12:04:43 +0800

    add core module

---
+++ royi-core/src/main/java/com/ruoyi/core/util/UUID32.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.royi.core.util;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class UUID32 {
+
+	public static String[] chars = new String[] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+			"o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8",
+			"9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+			"U", "V", "W", "X", "Y", "Z" };
+
+	public static String generateShortUuid() {
+		StringBuffer shortBuffer = new StringBuffer();
+		String uuid = UUID.randomUUID().toString().replace("-", "");
+		for (int i = 0; i < 8; i++) {
+			String str = uuid.substring(i * 4, i * 4 + 4);
+			int x = Integer.parseInt(str, 16);
+			shortBuffer.append(chars[x % 0x3E]);
+		}
+		return shortBuffer.toString();
+	}
+
+	public static String getUUID() {
+		String uuid = UUID.randomUUID().toString();
+		uuid = uuid.replaceAll("-", "");
+		return uuid;
+	}
+
+	public static void main(String[] args) {
+		Set<String> set = new HashSet<String>();
+		for (int i = 0; i < 1000000; i++) {
+			set.add(generateShortUuid());
+			// System.out.println(generateShortUuid());
+		}
+		System.out.println(set.size());
+	}
+
+	private UUID32() {
+
+	}
+}
